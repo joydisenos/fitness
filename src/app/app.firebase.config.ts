01fce53c1f490f78ac07f6af0e705f319b8dda3e
Joseph Vasquez <joydisenos@gmail.com>
2018-11-09 23:31:16 -0400

    cambios de configuracion

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBYaN3bY1HATgE28Lyzu4Mc0nnZOFFYAqw",
+    authDomain: "fitness-e4dff.firebaseapp.com",
+    databaseURL: "https://fitness-e4dff.firebaseio.com",
+    projectId: "fitness-e4dff",
+    storageBucket: "fitness-e4dff.appspot.com",
+    messagingSenderId: "535071090849"
+  }
